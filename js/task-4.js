d1424a41f4ac5811a675332837cdd3ad511eebe4
Alina_Khliebnikova <alinchiessss@gmail.com>
2026-04-25 18:28:51 +0200

    finish DOM tasks 1-5

---
+++ js/task-4.js
@@ -0,0 +1,24 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const elements = event.currentTarget.elements;
+
+  const email = elements.email.value.trim();
+  const password = elements.password.value.trim();
+
+  if (email === "" || password === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  const data = {
+    email: email,
+    password: password,
+  };
+
+  console.log(data);
+
+  form.reset();
+});
